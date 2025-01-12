f834bb2dd50270626410960b87e1bcad01b3e0a9
Killua <75320462+kenli123423@users.noreply.github.com>
2025-01-12 18:25:50 +0800

    Delete CNN implementation directory

--- CNN implementation/Im2col/im2col_lab.py
+++
@@ -1,9 +0,0 @@
-import cupy as cp 
-import time
-X = cp.random.randn(100, 50, 10)
-Y = cp.random.randn(50, 100, 20)
-start = time.time()
-a = cp.tensordot(X, Y, axes=([1,0],[0,1]))
-end=time.time()
-print(end-start)
-del X, Y
